c0ca7b91c9e088af9536ae309dc812034f1922f0
Sean Fagan <faganseanp@gmail.com>
2023-11-12 13:58:12 -0700

    added pacman with movement

--- index.js
+++ index.js
@@ -19,6 +19,58 @@ class Boundary {
   }
 }
 
+class Pacman {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 15;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "yellow";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+const boundaries = [];
+
+const pacman = new Pacman({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+let lastKey = "";
+
 const map = [
   ["-", "-", "-", "-", "-", "-"],
   ["-", " ", " ", " ", " ", "-"],
@@ -26,8 +78,6 @@ const map = [
   ["-", " ", " ", " ", " ", "-"],
   ["-", "-", "-", "-", "-", "-"],
 ];
-
-const boundaries = [];
 
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -46,6 +96,65 @@ map.forEach((row, i) => {
   });
 });
 
-boundaries.forEach((boundary) => {
-  boundary.draw();
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+  pacman.update();
+
+  pacman.velocity.y = 0;
+  pacman.velocity.x = 0;
+
+  if (keys.w.pressed && lastKey === "w") {
+    pacman.velocity.y = -5;
+  } else if (keys.a.pressed && lastKey === "a") {
+    pacman.velocity.x = -5;
+  } else if (keys.s.pressed && lastKey === "s") {
+    pacman.velocity.y = 5;
+  } else if (keys.d.pressed && lastKey === "d") {
+    pacman.velocity.x = 5;
+  }
+}
+
+animate();
+
+addEventListener("keydown", ({ key }) => {
+  // destructuring 'key' from event object
+  switch (key) {
+    case "w":
+      keys.w.pressed = true;
+      lastKey = "w";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "s":
+      keys.s.pressed = true;
+      lastKey = "s";
+      break;
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+  }
+});
+
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
 });
